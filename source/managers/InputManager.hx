f8f5a274a1cbb35a15aea2a8a06b258cc76fdc02
Karlo <Karlo@localhost>
2013-11-11 18:25:16 +0800

    If you have already dragged, do not click on a territory

--- source/managers/InputManager.hx
+++ source/managers/InputManager.hx
@@ -15,6 +15,8 @@ class InputManager extends FlxBasic
 {	
 	var startTouchPos : FlxPoint = null;
 	var screenDrag : Float = 0.5;
+	var distanceToDrag : Float = 5;
+	var isDragging:Bool = false;
 	
 	public function new() 
 	{
@@ -35,16 +37,16 @@ class InputManager extends FlxBasic
 		}
 		else if ( startTouchPos != null && FlxG.mouse.justReleased() )
 		{
-			if ( distanceFromStartTouch < 5 )
-			{
-				GameplayManager.onClick(FlxG.mouse.x, FlxG.mouse.y);
-			}
+			if ( !isDragging && distanceFromStartTouch < distanceToDrag )			
+				GameplayManager.onClick(FlxG.mouse.x, FlxG.mouse.y);			
 				
 			startTouchPos = null;
+			isDragging = false;
 		}
 		
-		if ( startTouchPos != null && distanceFromStartTouch > 5)
+		if ( startTouchPos != null && distanceFromStartTouch > distanceToDrag)
 		{			
+			isDragging = true;
 			FlxG.camera.scroll.x = (FlxG.camera.scroll.x + startTouchPos.x - FlxG.mouse.screenX) * screenDrag;
 			FlxG.camera.scroll.y = (FlxG.camera.scroll.y + startTouchPos.y - FlxG.mouse.screenY) * screenDrag; 
 		}
