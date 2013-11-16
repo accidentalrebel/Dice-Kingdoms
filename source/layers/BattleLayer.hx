258d541f45cb601d17de68655bff51be82dd20e3
Karlo <Karlo@localhost>
2013-11-17 00:32:12 +0800

    Created a battle GUI background

--- source/layers/BattleLayer.hx
+++ source/layers/BattleLayer.hx
@@ -1,4 +1,5 @@
 package layers;
+import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.group.FlxGroup;
 import flixel.text.FlxText;
@@ -14,9 +15,12 @@ class BattleLayer extends FlxGroup
 	public function new() 
 	{
 		super();
+
+		var bgWidth : Int = Std.int(FlxG.camera.width);
+		var bgHeight : Int = 200;
 		
-		var battleBackground : FlxSprite = new FlxSprite();
-		battleBackground = battleBackground.makeGraphic(200, 200, 0xff000000 );
+		var battleBackground : FlxSprite = new FlxSprite(0, FlxG.camera.height / 2 - bgHeight / 2);
+		battleBackground = battleBackground.makeGraphic(bgWidth, bgHeight, 0xff000000 );
 		battleBackground.alpha = 0.5;
 		add(battleBackground);
 		
