353a2038d6a84722a7b0052c23785359a2a55666
Karlo <Karlo@localhost>
2013-11-21 20:57:01 +0800

    Changed the gray color to black

--- source/Registry.hx
+++ source/Registry.hx
@@ -12,7 +12,7 @@ import managers.TerritoryManager;
  */
 class Registry
 {
-	public static var colorList : Array<Int> = [ 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0x888888, 0xFFFFFF ];		
+	public static var colorList : Array<Int> = [ 0xFF0000, 0x00FF00, 0x0000FF, 0xFFFF00, 0x00FFFF, 0xFF00FF, 0x000000, 0xFFFFFF ];		
 	public static var maxTerritories : Int = Math.floor(PlayAreaLayer.playAreaRows / 5) * Math.floor(PlayAreaLayer.playAreaCols / 5);	
 	static public var territoryPerPlayer:Int;
 	
