ed1f9f2c016a135eb9e7a24c75845b4cfa72fa2b
Karlo <Karlo@localhost>
2013-11-25 22:42:17 +0800

    Adjusted the buttons for easy testing on mobiles

--- source/layers/GameGUILayer.hx
+++ source/layers/GameGUILayer.hx
@@ -21,18 +21,18 @@ class GameGUILayer extends FlxGroup
 	{
 		super();
 		
-		var buttonHeight : Int = 30;
+		var buttonHeight : Int = 60;
 		
 		playerIndicator = new FlxText(5, 5, 100, "Player 1", 16);
 		add(playerIndicator);
-		var doneButton : FlxButtonPlus = new FlxButtonPlus(5, (buttonHeight + 10), GameplayManager.nextPlayer, null, "DONE", 80, buttonHeight);		
+		var doneButton : FlxButtonPlus = new FlxButtonPlus(5, Std.int(playerIndicator.height + 10), GameplayManager.nextPlayer, null, "DONE", 80, buttonHeight);		
 		add(doneButton);
 		
-		var zoomButton : FlxButtonPlus = new FlxButtonPlus(5, (buttonHeight + 10) * 2, CameraManager.toggleZoom, null, "TOGGLE ZOOM", 80, buttonHeight);		
+		var zoomButton : FlxButtonPlus = new FlxButtonPlus(5, Std.int(playerIndicator.height + buttonHeight + 40), CameraManager.toggleZoom, null, "TOGGLE ZOOM", 80, buttonHeight);		
 		add(zoomButton);
 		
-		var resetButton : FlxButtonPlus = new FlxButtonPlus(5, (buttonHeight + 10) * 3, GameplayManager.resetGame, null, "NEW GAME", 80, buttonHeight);		
-		add(resetButton);
+		//var resetButton : FlxButtonPlus = new FlxButtonPlus(5, (buttonHeight + 10) * 3, GameplayManager.resetGame, null, "NEW GAME", 80, buttonHeight);		
+		//add(resetButton);
 		
 		// Everything in this group does not move from the camera
 		this.setAll("scrollFactor", new FlxPoint(0, 0));
