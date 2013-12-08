07b34a2cd17d8d63ac27b7c07123f0d7dcee3083
Karlo <Karlo@localhost>
2013-12-08 19:02:51 +0800

    Can now display the other labels in the playerRow

--- source/misc/PlayerRow.hx
+++ source/misc/PlayerRow.hx
@@ -8,7 +8,7 @@ import flixel.text.FlxText;
  * ...
  * @author Karlo
  */
-class PlayerRow extends FlxSprite
+class PlayerRow
 {
 	private var nameLabel 	: FlxText;
 	private var aiLabel 	: FlxText;
@@ -16,13 +16,20 @@ class PlayerRow extends FlxSprite
 	private var parent:FlxSpriteGroup;
 
 	public function new(tParent : FlxSpriteGroup, xPos : Float, yPos : Float) 
-	{
-		super();
-		
+	{		
 		parent = tParent;
-		nameLabel = new FlxText(xPos, yPos, 300, "Player Label", 14);
+		nameLabel = new FlxText(xPos, yPos, 150, "Player Label", 14);
+		nameLabel.alignment = "center";
+		
+		aiLabel = new FlxText(xPos + 150, yPos, 100, "AI Label", 14);
+		aiLabel.alignment = "center";
+		
+		territoryCountLabel = new FlxText(xPos + 150 + 100, yPos, 150, "Territory Label", 14);
+		aiLabel.alignment = "center";
 		
 		parent.add(nameLabel);
+		parent.add(aiLabel);
+		parent.add(territoryCountLabel);
 	}
 	
 }
