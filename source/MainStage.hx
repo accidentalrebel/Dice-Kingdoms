c9c1f4c4bc95caf99e7023bdf8996227e3a27351
Karlo <Karlo@localhost>
2013-12-19 23:56:40 +0800

    Created a ScreenWidth and height variable in MainStage

--- source/MainStage.hx
+++ source/MainStage.hx
@@ -6,10 +6,11 @@ package ;
  */
 class MainStage
 {	
+	//TODO: Include instructions on how and where to use these variables with examples
 	static public var adjustedWidth:Float = 0;
 	static public var adjustedHeight:Float = 0;
 	static public var cameraHeight:Float = 0;
 	static public var cameraWidth:Float = 0;
-	
-	//TODO: Add a actual Screen size variables
+	static public var screenWidth:Float = 0;
+	static public var screenHeight:Float = 0;
 }
