1f0eea9d38432b14291d32908c103fb0f296b46e
Karlo <Karlo@localhost>
2013-12-20 20:27:21 +0800

    Edited the values of PlayerColor

--- source/misc/PlayerColor.hx
+++ source/misc/PlayerColor.hx
@@ -7,13 +7,13 @@ import flixel.util.FlxArrayUtil;
  */
 class PlayerColor
 {
-	inline public static var RED : Int = 0xFF3333;
-	inline public static var GREEN : Int = 0x33FF33;
-	inline public static var BLUE : Int = 0x3333FF;
-	inline public static var YELLOW : Int = 0xFFFF33;
-	inline public static var AQUA : Int = 0x33FFFF;
-	inline public static var PINK : Int = 0xFF33FF;
-	inline public static var ORANGE : Int = 0xFFA800;
+	inline public static var RED : Int = 0xFF0000;
+	inline public static var GREEN : Int = 0x00FF00;
+	inline public static var BLUE : Int = 0x0000FF;
+	inline public static var YELLOW : Int = 0xFFFF00;
+	inline public static var AQUA : Int = 0x00FFFF;
+	inline public static var PINK : Int = 0xFF00FF;
+	inline public static var ORANGE : Int = 0xFF5500;
 	inline public static var WHITE : Int = 0xFFFFFF;
 
 	public static var colorList : Array<Int> = [ PlayerColor.RED, PlayerColor.GREEN, PlayerColor.BLUE, PlayerColor.YELLOW
